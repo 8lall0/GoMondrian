72c568f3fa2f8a8618b8f535079a886446fcaac5
8lall0 <savino.liguori@gmail.com>
2017-09-20 13:17:06 +0200

    Added error checking.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/png"
 	"os"
 
@@ -9,6 +10,10 @@ import (
 
 func main() {
 	img, _ := gomondrian.Generate(300, 200, 7, 350, 30)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
