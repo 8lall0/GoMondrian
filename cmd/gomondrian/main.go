24dace8fa0bb3fb8a146affc766da78ce2aed8fe
8lall0 <savino.liguori@gmail.com>
2017-09-20 14:40:42 +0200

    Added flags to cmd.

--- cmd/gomondrian/main.go
+++ cmd/gomondrian/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image/png"
 	"os"
@@ -9,13 +10,22 @@ import (
 )
 
 func main() {
-	img, err := gomondrian.Generate(600, 400, 7, 350, 30)
+	width := flag.Int("x", 640, "set width")
+	height := flag.Int("y", 480, "set height")
+	padding := flag.Int("p", 10, "set padding")
+	divisions := flag.Int("d", 50, "set number of divisions")
+	colors := flag.Int("c", 30, "set number of colored squares")
+	filename := flag.String("s", "out.png", "set file output name")
+
+	flag.Parse()
+
+	img, err := gomondrian.Generate(*width, *height, *padding, *divisions, *colors)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	f, _ := os.OpenFile(*filename, os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
 	png.Encode(f, img)
 }
